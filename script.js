4585eadab9e59006e64663d9c2d30e8de2700dcd
mufadah <mufadah1350@gmail.com>
2024-04-25 21:16:30 +0700

    git config

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //ini file js
+let isReverse = "true"
+
+function reverse() {
+    let cToF = document.getElementById('c-to-f');
+    let fToC = document.getElementById('f-to-c');
+    
+    if (isReverse) {
+        console.log (isReverse)
+        fToC.style.display ="block";
+        cToF.style.display ="none";
+        isReverse = false
+    } else {
+        console.log (isReverse)
+        cToF.style.display ="block";
+        fToC.style.display ="none";
+        isReverse = true
+    }
+
+}
+
+function convert() {
+    let celcius = document.getElementById("c-to-f-input").Value;
+    if (isNaN(celcius)) {
+        alert("tolong input nomer");
+    }
+    console.log(celcius);
+}
+
+document.getElementById("button-convert").addEventListener('click', () => convert());
